02a7ea997d6efcf398cf458b65a77b213d3a738d
Siva-Sai-Deepak-Pulipaka <18l11a0518@gmail.com>
2023-04-11 16:43:59 +0530

    spell check

--- vars/common.groovy
+++ vars/common.groovy
@@ -20,7 +20,7 @@ def testcases() {
 }
 
 def codequality() {
-    withAWSParameters(credentialsId: 'PARAM', naming: 'absolute', path: 'sonarqube.user', recursive: true, regionName: 'us-east-1') 
+    withAWSParameterStore(credentialsId: 'PARAM', naming: 'absolute', path: '/sonarqube', recursive: true, regionName: 'us-east-1') 
     {
       sh 'sonar-scanner -Dsonar.host.url=http://172.31.13.99:9000 -Dsonar.login=${SONARQUBE_USER} -Dsonar.password=${SONARQUBE_PASS} -Dsonar.projectKey=${component} ${sonar_extra_opts}'
     }
