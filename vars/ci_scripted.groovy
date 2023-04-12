3fa2f3904048d020174a57f25f953f609ca80152
Siva-Sai-Deepak-Pulipaka <18l11a0518@gmail.com>
2023-04-12 18:41:22 +0530

    condition declared

--- vars/ci_scripted.groovy
+++ vars/ci_scripted.groovy
@@ -6,6 +6,13 @@ def call() {
         env.sonar_extra_opts = ""
     }
 
+
+    if(env.TAG_NAME ==~ ".*") {
+    env.GTAG = "true"
+    }   
+    else {
+    env.GTAG = "false"
+    }
     node('workstation') {
 
         try {
@@ -24,11 +31,14 @@ def call() {
              }
             }
 
-            
-            if(env.TAG_NAME)
-            stage('Test Cases') {
-                common.testcases()
-                }
+        println GTAG
+        println BRANCH_NAME
+
+        if(env.GTAG != "true" && env.BRANCH_NAME != "main") {
+        stage('Test Cases') {
+          common.testcases()
+            }
+        }
             
             stage('Code Quality') {
                         common.codequality()
