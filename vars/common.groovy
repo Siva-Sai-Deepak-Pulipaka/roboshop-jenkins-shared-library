74d94a45b696b03413296b9702f33041663e52ed
deepak <18l11a0518@gmail.com>
2023-04-13 17:35:04 +0530

    created artifact for maven

--- vars/common.groovy
+++ vars/common.groovy
@@ -32,6 +32,9 @@ def prepareArtifacts() {
     if (app_lang == "nodejs" || app_lang == "angular") {
     sh 'zip -r ${component}-${TAG_NAME}.zip * -x Jenkinsfile'
     }
+    if (app_lang == "maven") {
+        sh 'zip -r ${component}-${TAG_NAME}.zip ${component}.jar VERSION'
+    }
 
 }
 
